69c303b73c6db0d75d08c6bbb1bd9fca2191fd07
wenhuihuang <759547235@qq.com>
2016-10-15 13:16:30 +0800

    更新

---
+++ js/DialogB.js
@@ -0,0 +1,90 @@
+(function ($) {
+    function DialogB(options) {
+        this.dialogWrap = $('.dialog-wrap').length  > 0 ? $('.dialog-wrap') : $('<div class="dialog-wrap">');
+        this.settings = $.extend({
+            url:'test.html',
+            width:800,
+            height:460,
+            border:"1px solid red"
+        },options);
+        this.readerDom();
+        this.iframe=$('.iframe');
+        this.closeBtn = $('.close-btn');
+        this.bindEvent();
+    }
+    DialogB.prototype = {
+        constructor : DialogB,
+        readerDom : function () {
+            var self = this;
+            if($('.dialog-con').length < 1  ){
+                var html = '<div class="dialog-con">'+
+                    '<span class="close-btn"></span>'+
+                    '<iframe  frameborder="0" scrolling=no class="iframe" src="'+self.settings.url+'" frameborder="0" style="width:100%;height:100%;"></iframe>'+
+                    '</div>';
+                this.dialogWrap.html(html);
+                $('body').append(this.dialogWrap);
+            }else{
+                $('.iframe').attr('src',self.settings.url);
+            }
+
+
+            $('.dialog-wrap').css({
+                zIndex: 12000,
+                position:"fixed",
+                height:"100%",
+                width:"100%",
+                top:0,
+                bottom:0,
+                left:0,
+                right:0,
+                overflow: "auto",
+                background: "rgba(255, 255, 255, 0.75)",
+                display:"none"
+            })
+            $('.dialog-con').css({
+                background:"#fff",
+                position:"absolute",
+                width:self.settings.width,
+                height:self.settings.height,
+                top:"140px",
+                left:0,
+                right:0,
+                margin:"auto",
+                marginBottom:'20px',
+                "box-sizing": "border-box",
+                "-webkit-box-sizing":"border-box",
+                "-ms-box-sizing":"border-box",
+                "-o-box-sizing":"border-box",
+                "-moz-box-sizing":"border-box",
+                "border":self.settings.border,
+                "-webkit-border-radius": "6px",
+                "-moz-border-radius": "6px",
+                "border-radius": "6px"
+            })
+            $('.close-btn').css({
+                display: "inline-block",
+                width:"20px",
+                height:"21px",
+                position: "absolute",
+                right:"20px",
+                top:"20px",
+                cursor: "pointer",
+                background:"url(img/close_icon.gif) no-repeat center center"
+            })
+        },
+        show:function () {
+            this.dialogWrap.show();
+        },
+        hide:function () {
+            this.iframe.attr('src','');
+            this.dialogWrap.hide();
+        },
+        bindEvent:function () {
+            var self = this;
+            this.closeBtn.bind("click",function () {
+                self.hide();
+            })
+        }
+    }
+    window.DialogB=window['DialogB']=DialogB;
+})(jQuery)
